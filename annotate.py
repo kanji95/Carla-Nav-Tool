f214a01eefa919c7b3ab1ea97e66c288a8438ac7
Varun Chhangani <varunchhangani@gmail.com>
2022-01-28 14:31:34 +0530

    not working annotation

---
+++ annotate.py
@@ -0,0 +1,78 @@
+from email.policy import default
+import numpy as np
+import cv2
+import argparse
+import os
+import sys
+import shutil
+
+# K is a (3 x 3) matrix
+# world_points is a (3 x m) matrix,
+# where is the number of world points
+# returns a (2 x m) matrix
+def world_to_image(K, world_points, image_height, image_width):
+    K[0][2] = image_height / 2
+    K[1][2] = image_width / 2
+    image_points = K @ world_points.T
+    image_points = np.true_divide(image_points[0:2, :], image_points[[-1], :])
+    return image_points
+
+def annotate(args):
+    os.makedirs(args.out_dir,exist_ok=True)
+
+    for out_file in os.listdir(args.out_dir):
+        shutil.rmtree(os.path.join(args.out_dir,out_file))
+
+    episodes = os.listdir(args.dir)
+    print(episodes)
+    for episode in episodes:
+        print(f'Episode {episode}')
+        with open(os.path.join(args.dir,episode,'vehicle_positions.txt')) as f:
+            coordinates = f.readlines()
+        float_coordinates = np.array([[float(x.strip()) for x in last_coordinate.split(',')] for last_coordinate in coordinates])
+        target_coordinate = float_coordinates[-1]
+
+        relative_coords = target_coordinate - float_coordinates
+
+        K = np.load(os.path.join(args.dir,episode,'camera_intrinsic.npy'))
+
+        annotations = world_to_image(K,relative_coords,args.height,args.width).T
+
+        frames = sorted(os.listdir(os.path.join(args.dir,episode)))
+
+        for i,frame in enumerate(frames):
+            annotation = annotations[i]
+            print(annotation)
+            im = cv2.imread(os.path.join(args.dir,episode,frame))
+            im = cv2.circle(im, (annotation[1],annotation[0]), 3, (0,255,0),thickness=-1)
+            cv2.imwrite(os.path.join(args.out_dir,episode,frame),im)
+
+        K = np.load(os.path.join(args.dir,episode,'vehicle_positions.txt'))
+
+        image = cv2.imread()
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='VLN data annotater')
+    argparser.add_argument(
+        '-d', '--dir',
+        default='_out/',
+        help='Input Data Directory Path')
+    argparser.add_argument(
+        '-o', '--out_dir',
+        default='_annotated_out',
+        help='Output Data Directory Path')
+    argparser.add_argument(
+        '-n', '--height',
+        default=720,
+        help='input image height')
+    argparser.add_argument(
+        '-m', '--width',
+        default=1280,
+        help='input image width')
+    args = argparser.parse_args()
+    annotate(args)
+
+if __name__=='__main__':
+    main()
